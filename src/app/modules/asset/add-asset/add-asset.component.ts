9ebf4427ff41974c7814a9088be8c760b3281a0e
Spencer Plasse <spencer.plasse@cgi.com>
2022-10-12 00:32:25 -0500

    "asset/add" form implementation and logging updates

--- src/app/modules/asset/add-asset/add-asset.component.ts
+++ src/app/modules/asset/add-asset/add-asset.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Asset } from '@/models/asset';
+import { AssetService } from '@/services/asset.service';
+import { LogService } from '@/services/log.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'add-asset',
   templateUrl: './add-asset.component.html',
   styleUrls: ['./add-asset.component.css']
 })
-export class AddAssetComponent implements OnInit {
+export class AddAssetComponent{
+  asset: Asset = new Asset();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private assetService: AssetService, private logService: LogService, private router: Router){
+    assetService.getNextAssetId().subscribe(id => this.asset.assetTagId = id);
   }
 
+  addAsset(){
+    this.assetService.createAsset(this.asset).subscribe(asset => this.asset = asset);
+    this.logService.success("Successfully created a new asset!", this.asset);
+    this.router.navigate(["/asset", "details", this.asset.assetTagId]);
+  }
+
+  cancel(){
+    this.router.navigate(["/assets", "all"]);
+  }
 }
